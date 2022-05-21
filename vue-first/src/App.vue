c2d3e71eadec3b2ad2e2339dc8ce1d7802e19205
tBrause <brause.torsten@gmail.com>
2022-05-22 00:00:58 +0200

    y

--- vue-first/src/App.vue
+++ vue-first/src/App.vue
@@ -1,20 +1,41 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <hr />
+  {{ `Hi, ${first_name}` }}<br />
+  {{ fullName }}
+
+  <!--
+  {{ age > 18 ? "voll" : "nicht voll" }}
+  -->
+
+  <div class="age">
+    <span v-if="age < 18">nicht voll</span>
+    <span v-else-if="age == 18">voll</span>
+    <span v-else>Ü18</span>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  data: () => ({
+    first_name: "Torsten",
+    last_name: "Brause",
+    age: 51,
+  }),
+  computed: {
+    fullName() {
+      return `${this.first_name} ${this.last_name}`;
+    },
   },
 };
 </script>
 
 <style>
+.age {
+  border: 1px solid #666;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
